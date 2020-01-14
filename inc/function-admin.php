0a3f88de1c6eb23e32fcbad1a307c45e90abda93
Miillky <vedran.milkovic25@gmail.com>
2020-01-14 18:11:29 +0100

    Lesson 4

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -14,8 +14,26 @@ function sunset_add_admin_page(){
 	add_submenu_page('miillky_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'miillky_sunset', 'sunset_theme_create_page' );
 	add_submenu_page('miillky_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'miillky_sunset_css', 'sunset_theme_settings_page' );
 
+	//Activate custom settings
+	add_action('admin_init', 'sunset_custom_settings');
+
 }
 add_action('admin_menu', 'sunset_add_admin_page');
+
+function sunset_custom_settings(){
+	register_setting('sunset-settings-group', 'first_name' );
+	add_settings_section('sunset-sidebar-options', 'Sidebar Options', 'sunset_sidebar_options', 'miillky_sunset' );
+	add_settings_field('sidebar-name', 'First Name', 'sunset_sidebar_name', 'miillky_sunset', 'sunset-sidebar-options' );
+}
+
+function sunset_sidebar_options(){
+	echo 'Customize your Sidebar Information';
+}
+
+function sunset_sidebar_name(){
+	$firstName = esc_attr(get_option('first_name'));
+	echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name" />';
+}
 
 function sunset_theme_create_page(){
 	require_once get_template_directory() . '/inc/templates/sunset-admin.php';
