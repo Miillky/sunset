351493b442e38e89bc9de418cc3311a12fda1d29
Miillky <vedran.milkovic25@gmail.com>
2020-01-14 22:27:05 +0100

    Lesson 7

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -21,6 +21,7 @@ function sunset_add_admin_page(){
 add_action('admin_menu', 'sunset_add_admin_page');
 
 function sunset_custom_settings(){
+	register_setting('sunset-settings-group', 'profile_picture');
 	register_setting('sunset-settings-group', 'first_name' );
 	register_setting('sunset-settings-group', 'last_name');
 	register_setting('sunset-settings-group', 'description');
@@ -30,6 +31,7 @@ function sunset_custom_settings(){
 
 	add_settings_section('sunset-sidebar-options', 'Sidebar Options', 'sunset_sidebar_options', 'miillky_sunset' );
 
+	add_settings_field('sidebar-profile-picture', 'Profile Picture', 'sunset_sidebar_profile', 'miillky_sunset', 'sunset-sidebar-options' );
 	add_settings_field('sidebar-name', 'Full Name', 'sunset_sidebar_name', 'miillky_sunset', 'sunset-sidebar-options' );
 	add_settings_field('sidebar-description', 'Description', 'sunset_sidebar_description', 'miillky_sunset', 'sunset-sidebar-options' );
 	add_settings_field('sidebar-twitter', 'Twitter handler', 'sunset_sidebar_twitter', 'miillky_sunset', 'sunset-sidebar-options' );
@@ -39,6 +41,12 @@ function sunset_custom_settings(){
 
 function sunset_sidebar_options(){
 	echo 'Customize your Sidebar Information';
+}
+
+function sunset_sidebar_profile(){
+	$picture = esc_attr(get_option('profile_picture'));
+	echo '<input type="button" class="button button-secondary" value="Upload Profile Picture" id="upload-button" />
+		  <input type="hidden" id="profile-picture" name="profile_picture" value="'.$picture.'" />';
 }
 
 function sunset_sidebar_name(){
