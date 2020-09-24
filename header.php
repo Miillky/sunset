ac21e51959e8b35da37ee0a132550e5ae60ef96c
Miillky <vedran.milkovic25@gmail.com>
2020-09-24 22:22:38 +0200

    Lesson 18

--- header.php
+++ header.php
@@ -37,7 +37,8 @@
                                     wp_nav_menu([
                                         'theme_location'    => 'primary',
                                         'container'         => false,
-                                        'menu_class'        => 'nav navbar-nav'
+                                        'menu_class'        => 'nav navbar-nav',
+                                        'walker'            => new Sunset_Walker_Nav_Primary()
                                     ]);
                                 ?>
                             </nav>
