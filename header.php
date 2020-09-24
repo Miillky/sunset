cee5e2989251bd2f8a4378701b3e99a9d2c0aa38
Miillky <vedran.milkovic25@gmail.com>
2020-09-24 20:57:06 +0200

    Lesson 16

--- header.php
+++ header.php
@@ -7,15 +7,16 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<?php if(is_singular() && pings_open(get_queried_object())): ?>
-			<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-		<?php endif; ?>
-        <title><?php bloginfo('title'); ?></title>
-		<?php wp_head();  ?>
-	</head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="description" content="<?php bloginfo('description'); ?>">
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <?php if(is_singular() && pings_open(get_queried_object())): ?>
+            <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        <?php endif; ?>
+        <title><?php bloginfo('name'); wp_title(); ?></title>
+        <?php wp_head();  ?>
+    </head>
 	<body <?php body_class(); ?>>
         <div class="container">
             <div class="row">
